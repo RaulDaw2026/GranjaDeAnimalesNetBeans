3fbbfdefe8d7c893961d7dc8ec96cd8cb536138b
RaulDaw2026 <465368@alu.murciaeduca.es>
2026-03-04 10:47:25 +0100

    Añadido JavaDoc constructor clase Animal

--- src/animales/Animal.java
+++ src/animales/Animal.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package animales;
-//Cambio-prueba
+//Cambio-prueba2
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.Objects;
